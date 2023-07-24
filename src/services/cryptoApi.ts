d5c1f18e38a7b44b912dde7cdbeb54f372bbc7cf
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-07-24 19:43:36 +0600

    refactor: update api service file

--- src/services/cryptoApi.ts
+++ src/services/cryptoApi.ts
@@ -39,22 +39,3 @@ export const cryptoApi = createApi({
 });
 
 export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } = cryptoApi;
-
-// const options = {
-//   method: 'GET',
-//   url: 'https://coinranking1.p.rapidapi.com/coins',
-//   params: {
-//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
-//     timePeriod: '24h',
-//     'tiers[0]': '1',
-//     orderBy: 'marketCap',
-//     orderDirection: 'desc',
-//     limit: '50',
-//     offset: '0'
-//   },
-//   headers: {
-//     'X-RapidAPI-Key': 'fecb1ebb34msh0532c57e8a2deb3p1c8245jsne3742372431c',
-//     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-//   }
-// };
-// url: 'https://coinranking1.p.rapidapi.com/coins',
